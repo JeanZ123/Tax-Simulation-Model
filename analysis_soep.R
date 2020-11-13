7f6a2b2e2b2666565797a4b172ee6c212bf536d0
JeanZ123 <38879646+JeanZ123@users.noreply.github.com>
2020-11-14 00:49:41 +0100

    Update analysis_soep.R

--- analysis_soep.R
+++ analysis_soep.R
@@ -14,10 +14,12 @@ wealth <-
 
 hbrutto <- hbrutto * hbrutto$w11102
 
-# Calculate inequality measures for income
+# Calculate inequality measures for gross household income distribution
 
 gini_pretax <- Gini(hbrutto$i11101, corr = FALSE, na.rm = TRUE)
 gini_posttax <- Gini(hbrutto$i11102, corr = FALSE, na.rm = TRUE)
+
+# Calculate inequality measures for gross household equivalised income
 
 for (i in hbrutto) {
     hbrutto$adults <- hbrutto$d11106 - (1 + hbrutto$d11107)
@@ -27,11 +29,13 @@ for (i in hbrutto) {
 
 gini_pretax <- Gini(hbrutto$equiv, corr = FALSE, na.rm = TRUE)
 
-# Calculate inequality measures for wealth
+# Calculate inequality measures for household wealth distribution
 
 # Weight cases by sampling weight
 
 est_sim <- est_sim * est_sim$biphrf
+
+# Impute amount of individual household wealth (using variables 'w0111a', 'w0111b', 'w0111c', 'w0111d', 'w0111e' as prescribed in the SOEP documentation)
 
 gini <- Gini(wealth$w0111a, corr = FALSE, na.rm = TRUE)
 gini <- gini + Gini(wealth$w0111b, corr = FALSE, na.rm = TRUE)
